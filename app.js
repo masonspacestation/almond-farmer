cfb4754bd7986a6daaf2b15e102881f911120f0c
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-03-22 23:05:35 -0600

    all functions functioning. begin labeling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("🥜 go nuts")
 let almonds = 0
 
 let farm = {
-  harvestRate: 10,
+  harvestRate: 100,
   shippingRate: 3,
 
   seasonHarvest: 0,
@@ -13,30 +13,34 @@ let farm = {
 let clickUpgrades = [
   {
     name: 'tractors',
-    price: 15,
+    emoji: '🚜',
+    price: 40,
     quantity: 0,
-    multiplier: 1
+    multiplier: 1.1
   },
   {
     name: 'trucks',
-    price: 10,
+    emoji: '🚛',
+    price: 25,
     quantity: 0,
-    multiplier: 1
+    multiplier: 1.5
   }
 ];
 
 let automaticUpgrades = [
   {
     name: 'bees',
+    emoji: '🐝',
     price: 50,
     quantity: 0,
-    multiplier: 20
+    multiplier: 2
   },
   {
     name: 'trees',
+    emoji: '🌳',
     price: 75,
     quantity: 0,
-    multiplier: 20
+    multiplier: 4
   }
 ];
 
@@ -45,7 +49,8 @@ updateStats()
 
 // SECTION player action
 function harvestAlmonds() {
-  almonds += 10
+  almonds += farm.harvestRate
+  farm.seasonHarvest += farm.harvestRate
   updateStats()
 }
 
@@ -54,33 +59,40 @@ function harvestAlmonds() {
 function upgradePerClick(resource) {
   // trucks and tractors
   let clickUpgrade = clickUpgrades.find(upgrade => upgrade.name == resource)
-  console.log("upgrade: ", resource)
+  // console.log("upgrade: ", resource)
 
-  if (almonds > 25) {
+  if (almonds > clickUpgrade.price) {
     clickUpgrade.quantity += 1,
-      almonds -= 25
+      almonds -= clickUpgrade.price,
+      (clickUpgrade.price += (1.05 * clickUpgrade.price)).toFixed(0),
+      (farm.harvestRate += (3 * clickUpgrade.quantity)).toFixed(0)
   } else {
     console.log("didn't upgrade ", resource)
   }
-  (farm.harvestRate * 1.2).toFixed(0)
+  // (farm.harvestRate * 1.2).toFixed(0)
   document.getElementById(`draw-${clickUpgrade.name}`).innerHTML = `<h2>${clickUpgrade.quantity}</h2>`
+  document.getElementById(`buy-${clickUpgrade.name}-btn`).innerHTML = `${clickUpgrade.emoji} ${(clickUpgrade.price).toFixed(0)}`
   updateStats()
+  console.log(farm.harvestRate);
+  console.log(clickUpgrade);
 }
 
 
 function upgradePerInt(resource) {
-
+  // bees and trees
   let intUpgrade = automaticUpgrades.find(upgrade => upgrade.name == resource)
-  console.log("upgrade: ", resource)
+  // console.log("upgrade: ", resource)
 
-  if (almonds > 25) {
-    intUpgrade.quantity += 1,
-      almonds -= 25
+  if (almonds > intUpgrade.price) {
+    almonds -= intUpgrade.price,
+      intUpgrade.quantity += 1,
+      (intUpgrade.price += (1.05 * intUpgrade.price)).toFixed(0),
+      (farm.shippingRate += (3 * intUpgrade.quantity)).toFixed(0)
   } else {
     console.log("didn't upgrade ", resource)
   }
-  (farm.shippingRate * 1.2).toFixed(0)
   document.getElementById(`draw-${intUpgrade.name}`).innerHTML = `<h2>${intUpgrade.quantity}</h2>`
+  document.getElementById(`buy-${intUpgrade.name}-btn`).innerHTML = `+ ${intUpgrade.emoji} ${(intUpgrade.price).toFixed(0)}`
   updateStats()
 }
 
@@ -91,10 +103,14 @@ function upgradePerInt(resource) {
 
 
 function updateStats() {
-  drawAlmondsSupply()
-  drawShippingRate()
-  drawHarvestRate()
-  drawSeasonHarvest()
+  // drawAlmondsSupply()
+  // drawShippingRate()
+  // drawHarvestRate()
+  // drawSeasonHarvest()
+  document.getElementById('almondsSupply').innerHTML = `<h2>${(almonds).toFixed(0)}</h2>`
+  document.getElementById('shipping-rate').innerHTML = `<h2>${(farm.shippingRate).toFixed(0)}</h2>`
+  document.getElementById('harvest-rate').innerHTML = `<h2>${(farm.harvestRate).toFixed(0)}</h2>`
+  document.getElementById('season').innerHTML = `<h5>Season Total: ${farm.seasonHarvest}</h5>`
 
   // toggleButtons()
 
@@ -107,26 +123,29 @@ function updateStats() {
 // SECTION diplay
 
 
-function drawAlmondsSupply() {
-  document.getElementById('almondsSupply').innerHTML = `<h2>${almonds}</h2>`
-}
-
-function drawShippingRate() {
-  // console.log('🚜 🚛 shipping upgrade purchased')
-  document.getElementById('shipping-rate').innerHTML = `<h2>${farm.shippingRate}</h2>`
-  // bees and trees increase amount per interval (for stretch, could also speed up the interval at milestones); tractors and trucks increase amount per click
-}
-
-function drawHarvestRate() {
-  // console.log('🐝 🌳 harvest upgrade purchased');
-  document.getElementById('harvest-rate').innerHTML = `<h2>${(farm.harvestRate).toFixed(0)}</h2>`
-}
 
 
-function drawSeasonHarvest() {
-  // console.log('📦📦📦 season harvest');
-  document.getElementById('season').innerHTML = `<h5>Season Total: ${farm.seasonHarvest}</h5>`
-}
+
+// function drawAlmondsSupply() {
+//   document.getElementById('almondsSupply').innerHTML = `<h2>${(almonds).toFixed(0)}</h2>`
+// }
+
+// function drawShippingRate() {
+//   // console.log('🚜 🚛 shipping upgrade purchased')
+//   document.getElementById('shipping-rate').innerHTML = `<h2>${(farm.shippingRate).toFixed(0)}</h2>`
+//   // bees and trees increase amount per interval (for stretch, could also speed up the interval at milestones); tractors and trucks increase amount per click
+// }
+
+// function drawHarvestRate() {
+//   // console.log('🐝 🌳 harvest upgrade purchased');
+//   document.getElementById('harvest-rate').innerHTML = `<h2>${(farm.harvestRate).toFixed(0)}</h2>`
+// }
+
+
+// function drawSeasonHarvest() {
+//   // console.log('📦📦📦 season harvest');
+//   document.getElementById('season').innerHTML = `<h5>Season Total: ${farm.seasonHarvest}</h5>`
+// }
 
 
 // SECTION interval
